3cca03ae402e4621f9f28362bf8ab36484da489f
Luciano Tatsumi Teruia <tatsumiteruia@gmail.com>
2023-12-16 00:16:07 -0300

    Desafio: 'Criando um Sistema e Abstraindo um Celular com POO em C#' concluído. (Versão 1.0) Lembrete: talvez, tentar melhorar o código quando terminar o bootcamp.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Alguns testes realizados com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia(numero: "123", modelo: "Modelo 0", imei: "1111", memoria: 256);
+Console.WriteLine($"Smartphone da Nokia de número {n1.Numero}:");
+n1.Ligar();
+n1.InstalarAplicativo("Pokémon GO");
+n1.ReceberLigacao();
+n1.Numero = "123";
+Console.WriteLine($"O número do smartphone da Nokia foi alterado para: {n1.Numero}");
+
+Console.WriteLine("\n");
+
+
+Iphone i1 = new Iphone(numero: "456", modelo: "Modelo 1", imei: "2222", memoria: 128);
+Console.WriteLine($"Smartphone do Iphone de número {i1.Numero}:");
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Genshin Impact");
+i1.Ligar();
+i1.Numero = "123";
+Console.WriteLine($"O número do smartphone do Iphone foi alterado para: {i1.Numero}");
